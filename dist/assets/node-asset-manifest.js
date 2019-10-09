19fcce6b6764ea708210ca36bc1e2407d72404d5
TBraner <t.braner@triggerco.de>
2019-10-09 15:58:23 +0200

    🔥 removed dist

--- dist/assets/node-asset-manifest.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-define('test-ember-engines-with-vendor-addons/config/node-asset-manifest', function() {
-  return {
-    default: {"bundles":{"source-code-editor":{"assets":[{"uri":"/engines-dist/source-code-editor/assets/engine-vendor.js","type":"js"},{"uri":"/engines-dist/source-code-editor/assets/engine.js","type":"js"}]}}}
-  };
-});
--- dist/engines-dist/source-code-editor/config/environment.js
+++
@@ -1,5 +0,0 @@
-define('source-code-editor/config/environment', function() {
-  return {
-    default: {"modulePrefix":"source-code-editor"}
-  };
-});
